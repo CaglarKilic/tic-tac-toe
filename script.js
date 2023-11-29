4d9f3a7c8e9f27a2c26e6a6a010b9a357e70ac37
Caglar Kilic <73300418+CaglarKilic@users.noreply.github.com>
2023-11-29 20:21:50 +0300

    Finished for now. Gonna add pc opponent and fancy animations later.

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const board = (function () {
     const to2D = index => [Math.trunc(index / 3), index % 3];
 
     const reset = () => layout.map(row => row.fill('-'));
-    
+
+    const print = () => console.log(layout.flat().join('').replace(/.{3}/g, '$&\n'));
+
     const checkDraw = () => !layout.flat().includes('-');
+
+    // const emptyIndices = () => [...layout.flat().join('').matchAll(/-/g)].map(elem => elem['index']);
 
     function checkWin() {
         const reStraight = /xxx|ooo/;
@@ -31,8 +35,75 @@ const board = (function () {
         const [row, col] = to2D(index);
         if (layout[row][col] == '-') {
             layout[row][col] = sign;
+            return true;
         }
+        return false;
     }
 
-    return { place, checkWin, checkDraw, reset};
+    return { place, checkWin, checkDraw, reset, print };
+})();
+
+const game = (function () {
+    let player = 'x';
+
+    const changePlayer = () => player = player == 'x' ? 'o' : 'x';
+
+    const reset = () => player = 'x';
+
+    const getPlayer = () => player;
+
+    function playTurn(index) {
+        const valid = board.place(index, player);
+        if (valid) {
+            const win = board.checkWin();
+            if (win) {
+                return 1;
+            }
+
+            const draw = board.checkDraw();
+            if (draw) {
+                return 0;
+            }
+
+            changePlayer();
+        }
+        return -1;
+    }
+
+    return { playTurn, reset, getPlayer };
+
+})();
+
+(function () {
+    const cells = document.querySelectorAll('table#gameboard td');
+    const table = document.querySelector('table#gameboard');
+    cells.forEach(cell => cell.addEventListener('click', write));
+
+    function reset(event) {
+        event.stopPropagation();
+        cells.forEach(cell => cell.textContent = '');
+        table.removeEventListener('click', reset, true);
+        game.reset();
+        board.reset();
+    }
+
+    function write(event) {
+        event.stopPropagation();
+
+        const index = this.dataset.index;
+        this.textContent = game.getPlayer();
+
+        const end = game.playTurn(index);
+        if (end != -1) {
+            const score = document.getElementById(game.getPlayer());
+            const draw = document.getElementById('draw');
+            if (end) {
+                score.textContent = parseInt(score.textContent) + 1;
+            } else {
+                draw.textContent = parseInt(draw.textContent) + 1;
+            }
+
+            table.addEventListener('click', reset, true);
+        }
+    }
 })();
